698bb630f6f5eded09c8790b4970af1a27d2fca7
Thiago Jurados <dev.thiagoj@gmail.com>
2024-03-11 12:55:33 -0300

    feat: add quit game button

---
+++ Assets/Scripts/UI/QuitButton.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitButton : MonoBehaviour
+{
+    [SerializeField] Button quitButton;
+
+    private void Awake()
+    {
+        if(!quitButton)
+            quitButton = GetComponent<Button>();
+
+        quitButton.onClick.AddListener(Quit);
+    }
+
+    void Quit()
+    {
+#if UNITY_EDITOR
+        if (Application.isPlaying)
+        {
+            UnityEditor.EditorApplication.isPlaying = false;
+            return;
+        }
+#endif
+
+        Application.Quit();
+    }
+}
